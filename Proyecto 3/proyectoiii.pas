79d5ff903ed0b9db638f8f49f19301a9ed0da7eb
Cvethencourt <135485800+Cvethencourt@users.noreply.github.com>
2023-07-08 12:56:31 -0400

    Se crean los archivos para dar inicio al desarrollo
    Se crea un procedimiento que de la bienvenida al usuario

---
+++ Proyecto 3/proyectoiii.pas
@@ -0,0 +1,21 @@
+program thirdproyect;
+
+uses crt;
+var menu : integer;
+
+Procedure bienvenida;
+
+Begin
+	Writeln ('Bienvenido al sistema de reservas de Lidotel Hotel Boutique Margarita');
+	Writeln ('');
+	Writeln ('Aca podra realizar reservaciones, consultar reservaciones, entre otras');
+	writeln ('funciones de utilidad...');
+end;
+
+
+
+BEGIN
+bienvenida;	
+	
+END.
+
